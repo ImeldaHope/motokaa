ad87a650bddf78c5a4c4e1c85b2de881dc4bc949
IHope <31431447+ImeldaHope@users.noreply.github.com>
2024-06-28 12:21:31 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from '@/constants';
 import { HomeProps } from "@/types";
 
-export default async function Home({searchParams}) {
+export default async function Home({searchParams}: HomeProps) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || '',
     year: searchParams.year || 2022,
